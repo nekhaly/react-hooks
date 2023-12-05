46d57bf02066ac1eedaa94d4b5c0def05ba5f36f
Mohammed Elnekhaly <mohammed.elnekhaly@advancis.de>
2023-12-05 17:10:52 +0100

    Use useReducer hook
    Shows how the Redux thing work in brief.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
+import { useReducer } from 'react';
 import './App.css';
 
+const initialState = {
+  message: "Hello"
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "Yell":
+      return {
+        message: `${action.message}.. I have just said ${state.message}`
+      }
+    case "Whisper":
+      return {
+        message: `Excuse me! I have just said ${state.message}`
+      }
+    default:
+      return state;
+  }
+}; 
+
 function App() {
+  // Simple usage of useReducer, applying a reducer function on a number
+  const [number, setNumber] = useReducer((acc, newNumber) => acc + newNumber, 0);
+  
+  // The core of Redux pattern using useReducer function
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <section>
+      <div className='container'>
+        <h2>{ number }</h2>
+        <button onClick={() => setNumber(3)}>Reducer Accumulator</button>
+      </div>
+      <div className='flex-container'>
+        {state.message}
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <button onClick={() => dispatch({ type: "Yell", message: "WTF??" })}>Yell</button>
+          <button onClick={() => dispatch({ type: "Whisper" })}>Whisper</button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+    </section>
   );
 }
 
